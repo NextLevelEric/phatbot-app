9a3db696f88225d6f0aeb7ef7072282896b0381e
NextLevelEric <eric@nxtlm.com>
2026-09-04 12:02:39 -0400

    Add leaderboard privacy settings to Compete

--- src/app/compete/layout.tsx
+++ src/app/compete/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
 import NewAwardReveal from "@/components/NewAwardReveal";
+import LeaderboardIdentitySettings from "@/components/LeaderboardIdentitySettings";
 
 export default function CompeteLayout({children}:{children:ReactNode}){
-  return <><NewAwardReveal/>{children}</>;
+  return <><NewAwardReveal/>{children}<div className="mx-auto max-w-2xl px-4 pb-28 sm:px-6"><LeaderboardIdentitySettings/></div></>;
 }
