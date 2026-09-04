3226785eb537447a92d186ec487d32c38951c698
NextLevelEric <eric@nxtlm.com>
2026-09-04 12:58:32 -0400

    Mount Monday weekly kickoff in Compete

--- src/app/compete/layout.tsx
+++ src/app/compete/layout.tsx
@@ -1,8 +1,9 @@
 import type { ReactNode } from "react";
 import NewAwardReveal from "@/components/NewAwardReveal";
 import ChampionshipSunday from "@/components/ChampionshipSunday";
+import MondayKickoff from "@/components/MondayKickoff";
 import LeaderboardIdentitySettings from "@/components/LeaderboardIdentitySettings";
 
 export default function CompeteLayout({children}:{children:ReactNode}){
-  return <><NewAwardReveal/><ChampionshipSunday/>{children}<div className="mx-auto max-w-2xl px-4 pb-28 sm:px-6"><LeaderboardIdentitySettings/></div></>;
+  return <><NewAwardReveal/><MondayKickoff/><ChampionshipSunday/>{children}<div className="mx-auto max-w-2xl px-4 pb-28 sm:px-6"><LeaderboardIdentitySettings/></div></>;
 }
