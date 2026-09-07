ea407e7e717ca6f575f77a9ca17fab62dbc72ff9
NextLevelEric <eric@nxtlm.com>
2026-09-07 10:02:33 -0400

    Add PHATBOT workout report interpretation

--- src/app/sessions/[id]/report/layout.tsx
+++ src/app/sessions/[id]/report/layout.tsx
@@ -4,6 +4,7 @@ import type { ReactNode } from "react";
 import { useParams } from "next/navigation";
 import PlateauReportSignal from "./PlateauReportSignal";
 import ReportShareActions from "./ReportShareActions";
+import WorkoutReportClarity from "./WorkoutReportClarity";
 import WorkoutReportPRShareActions from "@/components/WorkoutReportPRShareActions";
 import TrainTogetherReportCard from "@/components/TrainTogetherReportCard";
 
@@ -14,6 +15,7 @@ export default function WorkoutReportLayout({ children }: { children: ReactNode 
     <>
       <PlateauReportSignal sessionId={params.id} />
       {children}
+      <WorkoutReportClarity />
       <TrainTogetherReportCard sessionId={params.id} />
       <ReportShareActions />
       <WorkoutReportPRShareActions sessionId={params.id} />
