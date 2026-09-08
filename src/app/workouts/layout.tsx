7a71b48db606ad1d432ff03b8104d67af990a291
NextLevelEric <eric@nxtlm.com>
2026-09-08 17:51:40 -0400

    Feature Eric program on workout picker

---
+++ src/app/workouts/layout.tsx
@@ -0,0 +1,5 @@
+import EricCurrentProgramCard from "@/components/EricCurrentProgramCard";
+
+export default function WorkoutsLayout({children}:{children:React.ReactNode}) {
+  return <><EricCurrentProgramCard />{children}</>;
+}
