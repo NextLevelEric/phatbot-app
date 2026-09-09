e3fb5f5e64bac39e19669b2ae32ae7da835ff33f
NextLevelEric <eric@nxtlm.com>
2026-09-09 07:56:22 -0400

    Mount Train Together room status on live sessions

---
+++ src/app/sessions/[id]/layout.tsx
@@ -0,0 +1,5 @@
+import TrainTogetherRoomBanner from "@/components/TrainTogetherRoomBanner";
+
+export default function SessionLayout({children}:{children:React.ReactNode}){
+ return <><TrainTogetherRoomBanner/>{children}</>;
+}
